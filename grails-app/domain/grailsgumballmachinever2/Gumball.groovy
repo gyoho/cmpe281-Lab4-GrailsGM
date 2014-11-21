cf7580dc78cd94f8f148cca739b612b945a26b06
Yaopeng Wu <yaopeng.wu@sjsu.edu>
2014-11-21 09:35:07 -0800

    implemented RESTful API

--- grails-app/domain/grailsgumballmachinever2/Gumball.groovy
+++ grails-app/domain/grailsgumballmachinever2/Gumball.groovy
@@ -1,8 +1,9 @@
 package grailsgumballmachinever2
 
+// expose a domain class as a REST resource
 import grails.rest.*
-
 @Resource(uri='/gumballs',formats=['json'])
+
 class Gumball {
 
     String modelNumber
